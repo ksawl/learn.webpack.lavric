cdcef4f446da4eb3c01c64374db6fe8f08939fbe
Serhii Karavashkin <js.karaone@gmail.com>
2020-06-29 21:53:46 +0300

    all config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/style.css'
+
+import $ from "jquery";
+import avg from "./some";
+
+$(".title").html("Hello3333");
+
+console.log("AVG: ", avg(1, 2, 4, 6));
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+let conf = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "main.js",
+        publicPath: "dist/",
+    },
+    devServer: {
+        overlay: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: "/node_modules/",
+            },
+            {
+                test: /.css$/,
+                use: ["style-loader","css-loader"]
+            }
+        ],
+    },
+};
+
+module.exports = (env, options) => {
+    let production = options.mode === "production";
+
+    conf.devtool = production ? false /** "source-map" */ : "eval-sourcemap";
+
+    return conf;
+};
